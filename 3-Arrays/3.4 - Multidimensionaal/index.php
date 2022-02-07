cbbadeb4af7c450153a6997dc8e750d696e0172a
colinwahlen2 <90839957+colinwahlen2@users.noreply.github.com>
2022-02-07 13:30:20 +0100

    wijzigingen

--- 3-Arrays/3.4 - Multidimensionaal/index.php
+++ 3-Arrays/3.4 - Multidimensionaal/index.php
@@ -3,6 +3,12 @@
 <body>
 
 <?php
+$abcGetallen = array(" a", " b", " c", [24, 56, 78, 90]);
+print_r ($abcGetallen);
+echo ($abcGetallen [0]);
+echo ($abcGetallen [3][1]);
+echo ($abcGetallen [2]);
+echo ($abcGetallen [3][3]);
 
 ?>
 
