9cd6a8080ebec72165d5e370662adde098626563
colinwahlen2 <90839957+colinwahlen2@users.noreply.github.com>
2022-02-07 12:49:46 +0100

    wijzigingen

--- 2-Variabelen-en-datatype/2.6 - gettype() 2/index.php
+++ 2-Variabelen-en-datatype/2.6 - gettype() 2/index.php
@@ -3,7 +3,17 @@
 <body>
 
 <?php
+$Test1 = "tekst";
+$Test2 = 123;
+$Test3 = 0.123;
+$Test4 = true;
+$Test5 = null;
 
+echo  gettype($Test1);
+echo  gettype($Test2);
+echo  gettype($Test3);
+echo  gettype($Test4);
+echo  gettype($Test5);
 ?>
 
 </body>
