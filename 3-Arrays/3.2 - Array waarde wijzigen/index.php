dee22347944d3feda61da0f6dcaea37404c0e195
colinwahlen2 <90839957+colinwahlen2@users.noreply.github.com>
2022-02-07 13:16:37 +0100

    wijzigingen

--- 3-Arrays/3.2 - Array waarde wijzigen/index.php
+++ 3-Arrays/3.2 - Array waarde wijzigen/index.php
@@ -3,6 +3,16 @@
 <body>
 
 <?php
+$MyDognames = array("<li>Ties</li>", "<li>Jurgen</li>", "<li>Jason</li>");
+echo $MyDognames[0];
+echo $MyDognames[1];
+echo $MyDognames[2];
+
+$MyDognames = array("<li>Ties2</li>", "<li>Jurgen2</li>", "<li>Jason2</li>");
+echo $MyDognames[0];
+echo $MyDognames[1];
+echo $MyDognames[2];
+
 
 ?>
 
