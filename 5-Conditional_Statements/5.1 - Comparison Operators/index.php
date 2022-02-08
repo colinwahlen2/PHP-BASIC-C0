c222929f4fb969ef963a7f5aec8947286863167d
colinwahlen2 <90839957+colinwahlen2@users.noreply.github.com>
2022-02-08 15:40:44 +0100

    wijzigingen

--- 5-Conditional_Statements/5.1 - Comparison Operators/index.php
+++ 5-Conditional_Statements/5.1 - Comparison Operators/index.php
@@ -3,6 +3,17 @@
 <body>
 
 <?php
+$test1 = 12;
+$test2 = "gewoon tekst";
+
+var_dump($test1 == $test2);
+var_dump($test1 === $test2);
+var_dump($test1 != $test2);
+var_dump($test1 > $test2);
+var_dump($test1 < $test2);
+var_dump($test1 <> $test2);
+var_dump($test1 >= $test2);
+var_dump($test1 <= $test2);
 
 ?>
 
