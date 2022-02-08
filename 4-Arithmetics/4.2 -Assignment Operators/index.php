7d733f5e69725db0bf57663bcf08663bd620050b
colinwahlen2 <90839957+colinwahlen2@users.noreply.github.com>
2022-02-08 14:37:27 +0100

    wijzigingen

--- 4-Arithmetics/4.2 -Assignment Operators/index.php
+++ 4-Arithmetics/4.2 -Assignment Operators/index.php
@@ -3,7 +3,20 @@
 <body>
 
 <?php
+$score = 0;
+echo "<li>$score</li>";
 
+$score += 10;
+echo "<li>$score</li>";
+
+$score -= 5;
+echo "<li>$score</li>";
+
+$score *= 4;
+echo "<li>$score</li>";
+
+$score /= 8;
+echo "<li>$score</li>";
 ?>
 
 </body>
