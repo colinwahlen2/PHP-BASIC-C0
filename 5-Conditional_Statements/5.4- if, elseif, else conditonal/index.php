36c31db001ae7fd8c0743c9606bf376021b2087d
colinwahlen2 <90839957+colinwahlen2@users.noreply.github.com>
2022-02-10 13:40:24 +0100

    wijzigingen

--- 5-Conditional_Statements/5.4- if, elseif, else conditonal/index.php
+++ 5-Conditional_Statements/5.4- if, elseif, else conditonal/index.php
@@ -3,7 +3,38 @@
 <body>
 
 <?php
+$prod_besteld = 9;
+if($prod_besteld < 10) {
+    $prijs = 1.50;
+} elseif($prod_besteld < 20) {
+    $prijs = 1.25;
+} else {
+    $prijs = 1.00;
+}
+echo $prijs;
 
+
+$prod_besteld = 11  ;
+if($prod_besteld < 10) {
+    $prijs = 1.50;
+} elseif($prod_besteld < 20) {
+    $prijs = 1.25;
+} else {
+    $prijs = 1.00;
+}
+
+echo $prijs;
+
+$prod_besteld = 22;
+
+if($prod_besteld < 10) {
+    $prijs = 1.50;
+} elseif($prod_besteld < 20) {
+    $prijs = 1.25;
+} else {
+    $prijs = 1.00;
+}
+echo $prijs;
 ?>
 
 </body>
