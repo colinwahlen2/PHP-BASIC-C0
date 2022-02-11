b2099e3871444dda9fe2360d660630ce41bf9f4e
colinwahlen2 <90839957+colinwahlen2@users.noreply.github.com>
2022-02-11 14:51:32 +0100

    wijzigingen

--- 6-Functions/6.1 - function declaration/index.php
+++ 6-Functions/6.1 - function declaration/index.php
@@ -3,7 +3,13 @@
 <body>
 
 <?php
-
+    function zetTweeWoordenAanElkaar(){
+        $woordje = "Hallo";
+        $zinnetje = "mooie blauwe planeet!";
+        $helezin = $woordje . " " . $zinnetje;
+        echo ($helezin);
+    } 
+    zetTweeWoordenAanElkaar();
 ?>
 
 </body>
