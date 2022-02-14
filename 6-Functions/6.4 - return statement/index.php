dcd7bf919d888bec8dd83d26c58eb0e048ebc22f
colinwahlen2 <90839957+colinwahlen2@users.noreply.github.com>
2022-02-14 16:23:21 +0100

    wijzigingen

--- 6-Functions/6.4 - return statement/index.php
+++ 6-Functions/6.4 - return statement/index.php
@@ -3,7 +3,16 @@
 <body>
 
 <?php
-
+function geefTienProcentKorting() {
+    $koopprijs = 75;
+    $korting = 10;
+    $korting = ($koopprijs / 100) * $korting;
+    return $koopprijs - $korting;
+}
+function totaleBedrag() {
+    echo ("Het totale bedrag is " . geefTienProcentKorting() . " Euro");
+}
+totaleBedrag()
 ?>
 
 </body>
