4b8ed7e56165c045b2c0f8ee102839cc0f9aabaa
colinwahlen2 <90839957+colinwahlen2@users.noreply.github.com>
2022-02-14 14:32:41 +0100

    wijzigingen

--- 6-Functions/6.2 - strlen()/index.php
+++ 6-Functions/6.2 - strlen()/index.php
@@ -3,7 +3,14 @@
 <body>
 
 <?php
-
+function mijnNaamIsGroot(){
+    $voornaam = "Colin";
+    $achternaam = "Wahlen";
+    $gehelenaam = "mijn naam is " . $voornaam . " " . $achternaam;
+    $gehelenaam = strtoupper($gehelenaam);
+    echo ($gehelenaam);
+}
+mijnNaamIsGroot();
 ?>
 
 </body>
