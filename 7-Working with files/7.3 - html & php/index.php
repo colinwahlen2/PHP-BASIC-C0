6eec9025eb63aa24de59dcef0ed81866456cb490
colinwahlen2 <90839957+colinwahlen2@users.noreply.github.com>
2022-02-15 15:33:15 +0100

    wijzigingen

--- 7-Working with files/7.3 - html & php/index.php
+++ 7-Working with files/7.3 - html & php/index.php
@@ -2,9 +2,13 @@
 <html>
 <body>
 
-<?php
-
-?>
+    <div id="IsMeerderJarig">
+    <?php
+        if($leeftijd < 18 ){
+            echo "<h1>Je bent te jong om deze pagina te zien</h1>";
+        }
+    ?>
+</div>
 
 </body>
 </html> 
